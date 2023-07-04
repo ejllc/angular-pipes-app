4c310b8ee12d715f31b86f2f6dcda89519ac94c3
ejllc <emiliano-javier.llorente-castelblanque@capgemini.com>
2023-07-04 11:26:43 +0200

    Slice Pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+
+  public name: string = 'Fernando';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Merlisa';
+    this.gender = 'female';
+  }
+
+  // i18n Plural
+
+  public clients: string[] = ['Maria', 'Pedro', 'Ramón', 'Fernando', 'Luis', 'Melisa'];
+  public clientsMap ={
+    '=0': 'no tenemos ningín cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos dos clientes esperando',
+    'other': 'tenemos # clientes esperando'
+  }
+
+  deleteClient(): void {
+    this.clients.pop();
+  }
 }
